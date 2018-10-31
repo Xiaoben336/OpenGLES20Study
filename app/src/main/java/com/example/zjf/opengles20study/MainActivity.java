e44072124a994ec90e7ed8a3367b35353704d94c
Xiaoben336 <614306572@qq.com>
2018-10-31 11:49:13 +0800

    绘制一个正方形

--- app/src/main/java/com/example/zjf/opengles20study/MainActivity.java
+++ app/src/main/java/com/example/zjf/opengles20study/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.zjf.opengles20study;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.zjf.opengles20study.glsv.SquareGLSurfaceView;
 import com.example.zjf.opengles20study.glsv.TriangleGLSurfaceView;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,6 +12,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(new TriangleGLSurfaceView(this)); // 绘制三角形
+        //setContentView(new TriangleGLSurfaceView(this)); // 绘制三角形
+        setContentView(new SquareGLSurfaceView(this)); // 绘制正方形
     }
 }
---
+++ app/src/main/java/com/example/zjf/opengles20study/glsv/SquareGLSurfaceView.java
@@ -0,0 +1,40 @@
+package com.example.zjf.opengles20study.glsv;
+
+import android.content.Context;
+
+import com.example.zjf.opengles20study.base.BaseGLSurfaceView;
+import com.example.zjf.opengles20study.shape.square.Square;
+
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
+
+/**
+*@description 绘制正方形的GLSurfaceView
+*
+*@author zjf
+*@date 2018/10/31 11:44
+*/
+public class SquareGLSurfaceView extends BaseGLSurfaceView{
+    public SquareGLSurfaceView(Context context) {
+        super(context);
+        setRenderer(new SquareRenderer()); // 绘制正方形
+    }
+
+    class SquareRenderer implements Renderer{
+        Square square;
+        @Override
+        public void onSurfaceCreated(GL10 gl, EGLConfig config) {
+            square = new Square();
+        }
+
+        @Override
+        public void onSurfaceChanged(GL10 gl, int width, int height) {
+            square.onSurfaceChanged(width,height);
+        }
+
+        @Override
+        public void onDrawFrame(GL10 gl) {
+            square.draw();
+        }
+    }
+}
